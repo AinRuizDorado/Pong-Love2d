f240a9400064aa39615c71ca4fd2ea2061d1a205
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2020-07-29 10:35:41 -0300

    ball collides with top and down and with the paddles

--- main.lua
+++ main.lua
@@ -22,6 +22,9 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- set the title of the app menu
+    love.window.setTitle('pong')
+    
     -- seed the RNG so that calls to random
     -- use the current time
     math.randomseed(os.time())
@@ -31,6 +34,7 @@ function love.load()
     -- https://fonts.google.com/specimen/Red+Rose?preview.text=PONG&preview.text_type=custom&preview.size=43
     textFont = love.graphics.newFont('RedRose-Bold.ttf', 13)
     scoreFont = love.graphics.newFont('RedRose-Bold.ttf', 32)
+    framesPerSecondFont = love.graphics.newFont('RedRose-Bold.ttf', 8)
     -- set to active font
     love.graphics.setFont(textFont)
     --[[setup screen takes a virtual WIDTH and a Virtual Height to rendering in a fixed screen of 1270 x 720 ]]
@@ -51,25 +55,58 @@ end
 
 -- love.update will execute at every frame, it takes a delta time witch is going to be executed a fraccion of a second
 function love.update(dt)
-    -- player 1 movement
-    if love.keyboard.isDown('w') then
-        player1.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('s') then
-        player1.dy = PADDLE_SPEED
-    else
-        player1.dy = 0
-    end
 
-    if love.keyboard.isDown('up') then
-        player2.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('down') then
-        player2.dy = PADDLE_SPEED
-    else
-        player2.dy = 0
-    end
 
     if gameState == 'play' then
         ball:update(dt)
+        if ball:collides(player1) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+            -- keep velocity going in the same direction, but randomize it
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+            -- keep velocity going in the same direction, but randomize it
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        -- detect upper and lower screen boundary collision and reverse it
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        if ball.y >= VIRTUAL_HEIGHT -4 then
+            ball.y = VIRTUAL_HEIGHT -4
+            ball.dy = -ball.dy 
+        end
+
+    -- player 1 movement
+        if love.keyboard.isDown('w') then
+            player1.dy = -PADDLE_SPEED
+        elseif love.keyboard.isDown('s') then
+            player1.dy = PADDLE_SPEED
+        else
+            player1.dy = 0
+        end
+
+        if love.keyboard.isDown('up') then
+            player2.dy = -PADDLE_SPEED
+        elseif love.keyboard.isDown('down') then
+            player2.dy = PADDLE_SPEED
+        else
+            player2.dy = 0
+        end
     end
 
     player1:update(dt)
@@ -133,6 +170,15 @@ function love.draw()
     -- ball render
     ball:render()
 
+    -- funciont that renders the fps
+    showFPS()
+
     -- end rendering the virtual resolution
     push:finish()
+end
+
+function showFPS()
+    love.graphics.setFont(framesPerSecondFont)
+    love.graphics.setColor(0/255, 255/255, 0/255, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
